d3b07c575c5a64ff12d458758308573ed1054170
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-09-02 19:06:53 +0200

    Added healthcheck

--- root/app/smartdns/healthcheck.sh
+++ root/app/smartdns/healthcheck.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
+IP=$(cat /app/openvpn/ip)
 
+var health 0
+
+for country in $(var VPN_COUNTRY) ; do
+
+    port=$(dict port $country)
+
+    VPNIP=$(wget http://api.ipify.org:80$port -O - -q 2>/dev/null)
+    RC=$?
+
+    if [ $RC -eq 1 ] || [ $RC":"$VPNIP = $IP ]; then
+        echo "$country: VPN down. "
+        var health 1
+    else
+        echo "$country: $VPNIP. "
+    fi
+done
+
+exit $(var health);
