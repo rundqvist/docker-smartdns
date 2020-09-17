588a2047b39d1dfeba5f18c1c4e4e117706c11f0
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-09-17 20:05:23 +0200

    Create mandatory.sh

---
+++ root/app/mandatory.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+for var in "VPN_PROVIDER" "VPN_USERNAME" "VPN_PASSWORD" "SMARTDNS_SERVICES" ; do 
+
+    if [ -z "$(var $var)" ] ; then
+        log -e smartdns "Environment variable '$var' is mandatory. "
+        var abort true
+    else
+        log -d smartdns "Mandatory variable '$var' is ok."
+    fi
+
+done
+
+if [ "$(var abort)" = "true" ] ; then
+    exit 1;
+fi
+exit 0;
