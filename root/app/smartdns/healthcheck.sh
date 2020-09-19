b37178beff4fa37b5e29f9006447d1ca87c4358f
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-09-19 08:44:56 +0200

    Added self healing

--- root/app/smartdns/healthcheck.sh
+++ root/app/smartdns/healthcheck.sh
@@ -2,24 +2,38 @@
 
 log -v smartdns "[health] Check health"
 
-IP=$(cat /app/openvpn/ip)
-
 var health 0
 
 for country in $(var VPN_COUNTRY) ; do
 
     port=$(var -k port $country)
 
-    VPNIP=$(wget http://api.ipify.org:80$port -O - -q 2>/dev/null)
-    RC=$?
+    VPNIP=$(wget http://api.ipify.org:80$port -T 10 -O - -q 2>/dev/null)
+    
+    if [ $? -eq 1 ] || [ "$VPNIP" = "$(var publicIp)" ]
+    then
+        var -k fail $country + 1
+        count=$(var -k fail $country)
 
-    if [ $RC -eq 1 ] || [ $RC":"$VPNIP = $IP ]; then
-        log -d smartdns "[health] $country: VPN down."
-        echo "$country: VPN down. "
+        log -d smartdns "[health] $country: VPN down ($count)."
+
+        echo "$country: VPN down ($count). "
+
+        if [ "$count" = "3" ]
+        then
+            var -d fail
+            log -i smartdns "Restarting $country VPN."
+            pid=$(ps -o pid,args | sed -n "/openvpn\/config-$country/p" | awk '{print $1}')
+
+            kill -s SIGHUP $pid
+        fi
+ 
         var health 1
     else
         log -d smartdns "[health] $country: $VPNIP."
         echo "$country: $VPNIP. "
+
+        var -k fail -d $country
     fi
 done
 
