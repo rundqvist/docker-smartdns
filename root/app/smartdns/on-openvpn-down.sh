626d3a2d0fea3ea28e89ed64100679e332454c98
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-10-05 18:46:59 +0200

    Implemented new log syntax

--- root/app/smartdns/on-openvpn-down.sh
+++ root/app/smartdns/on-openvpn-down.sh
@@ -8,24 +8,24 @@ port=$(var -k port $country)
 http=$(var -k vpn.range http)
 https=$(var -k vpn.range https)
 
-log -d smartdns "Remove routing for $country vpn: tun: $tun, ip: $ip, gateway: $gateway, port: $port."
+log -d "Remove routing for $country vpn: tun: $tun, ip: $ip, gateway: $gateway, port: $port."
 
 # -- Clean up old config
-log -v smartdns "Delete nat ($country): iptables -D POSTROUTING -t nat -o $tun -j MASQUERADE"
+log -v "Delete nat ($country): iptables -D POSTROUTING -t nat -o $tun -j MASQUERADE"
 iptables -D POSTROUTING -t nat -o $tun -j MASQUERADE
 
-log -v smartdns "Delete mangle ($country): iptables -D OUTPUT -t mangle -o eth0 -p tcp --dport $https$port -j MARK --set-mark 0x$port"
+log -v "Delete mangle ($country): iptables -D OUTPUT -t mangle -o eth0 -p tcp --dport $https$port -j MARK --set-mark 0x$port"
 iptables -D OUTPUT -t mangle -o eth0 -p tcp --dport $https$port -j MARK --set-mark 0x$port
 
-log -v smartdns "Delete mangle ($country): iptables -D OUTPUT -t mangle -o eth0 -p tcp --dport $http$port -j MARK --set-mark 0x$port"
+log -v "Delete mangle ($country): iptables -D OUTPUT -t mangle -o eth0 -p tcp --dport $http$port -j MARK --set-mark 0x$port"
 iptables -D OUTPUT -t mangle -o eth0 -p tcp --dport $http$port -j MARK --set-mark 0x$port
 
-log -v smartdns "Delete rule ($country): fwmark 0x$port table $tun"
+log -v "Delete rule ($country): fwmark 0x$port table $tun"
 ip rule del fwmark 0x$port table $tun
 
-log -v smartdns "Delete route ($country): default via $gateway dev $tun table $tun"
+log -v "Delete route ($country): default via $gateway dev $tun table $tun"
 ip route del default via $gateway dev $tun table $tun
 
-log -v smartdns "Delete routing table ($country): 20$port $tun"
+log -v "Delete routing table ($country): 20$port $tun"
 sed -i "/20$port $tun/d" /etc/iproute2/rt_tables
 # --
--- root/app/smartdns/on-openvpn-up.sh
+++ root/app/smartdns/on-openvpn-up.sh
@@ -8,24 +8,24 @@ port=$(var -k port $country)
 http=$(var -k vpn.range http)
 https=$(var -k vpn.range https)
 
-log -d smartdns "Setup routing for $country vpn: tun: $tun, ip: $ip, gateway: $gateway, port: $port."
+log -d "Setup routing for $country vpn: tun: $tun, ip: $ip, gateway: $gateway, port: $port."
 
 # -- Create new config --
-log -v smartdns "Add routing table ($country): 20$port $tun"
+log -v "Add routing table ($country): 20$port $tun"
 echo "20$port $tun" >> /etc/iproute2/rt_tables
 
-log -v smartdns "Add route ($country): default via $gateway dev $tun table $tun"
+log -v "Add route ($country): default via $gateway dev $tun table $tun"
 ip route add default via $gateway dev $tun table $tun
 
-log -v smartdns "Add rule ($country): fwmark 0x$port table $tun"
+log -v "Add rule ($country): fwmark 0x$port table $tun"
 ip rule add fwmark 0x$port table $tun
 
-log -v smartdns "Add mangle ($country): iptables -A OUTPUT -t mangle -o eth0 -p tcp --dport $http$port -j MARK --set-mark 0x$port"
+log -v "Add mangle ($country): iptables -A OUTPUT -t mangle -o eth0 -p tcp --dport $http$port -j MARK --set-mark 0x$port"
 iptables -A OUTPUT -t mangle -o eth0 -p tcp --dport $http$port -j MARK --set-mark 0x$port
 
-log -v smartdns "Add mangle ($country): iptables -A OUTPUT -t mangle -o eth0 -p tcp --dport $https$port -j MARK --set-mark 0x$port"
+log -v "Add mangle ($country): iptables -A OUTPUT -t mangle -o eth0 -p tcp --dport $https$port -j MARK --set-mark 0x$port"
 iptables -A OUTPUT -t mangle -o eth0 -p tcp --dport $https$port -j MARK --set-mark 0x$port
 
-log -v smartdns "Add nat ($country): iptables -A POSTROUTING -t nat -o $tun -j MASQUERADE"
+log -v "Add nat ($country): iptables -A POSTROUTING -t nat -o $tun -j MASQUERADE"
 iptables -A POSTROUTING -t nat -o $tun -j MASQUERADE
 # --
